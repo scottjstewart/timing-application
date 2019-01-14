e23cae5ba5cf94183d59a5af84aa3d111f8330a0
scottjstewart <scottstewart965@gmail.com>
2019-01-14 10:10:40 -0500

    initial commit for timing app

---
+++ src/components/site/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Row } from "reactstrap";
+
+const Footer = () => {
+  return (
+    <footer>
+      <Row>
+        <p>Timing App: Created by Scott Stewart</p>
+        <p>https://github.com/scottjstewart/new-project-prac</p>
+      </Row>
+    </footer>
+  );
+};
+
+export default Footer;
